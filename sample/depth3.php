5208fe91f93b52b7bb6fc873cb93f5e9532763a9
tamilamuthan <tamilamuthan@gmail.com>
2014-04-12 14:30:37 +0530

    initial files

---
+++ sample/depth3.php
@@ -0,0 +1,8 @@
+<?php
+$find="blah!";
+$me="blah!blah!";
+$where="blah!blah!blah!";
+$I="blah!blah!blah!blah!";
+$am="blah!blah!blah!blah!blah!";
+$abc=0
+?>
---
+++ sample/index.php
@@ -0,0 +1,4 @@
+<?php
+include_once(dirname(dirname(__FILE__))."/phptrace.php");
+include_once(dirname(__FILE__)."/depth1.php");
+?>
